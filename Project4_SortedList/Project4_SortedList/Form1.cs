675e14bd3aca4565b060980574a6eb31004a9a40
Avgustin16 <35662619+Avgustin16@users.noreply.github.com>
2018-10-15 15:42:15 +0300

    Add files via upload

---
+++ Project4_SortedList/Project4_SortedList/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Project4_SortedList
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(textBox1.Text);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (listBox1.Sorted == false)
+            {
+                listBox1.Sorted = true;
+                button2.Text = "Unsort";
+            }
+            else
+            {
+                listBox1.Sorted = false;
+                button2.Text = "Sort";
+            }
+        }
+    }
+}
