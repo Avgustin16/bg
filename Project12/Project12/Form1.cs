b69873ddb72ad3f461aeee2d281c56dee91b2967
Avgustin16 <35662619+Avgustin16@users.noreply.github.com>
2018-11-08 15:43:40 +0200

    Add files via upload

--- Project12/Project12/Form1.cs
+++ Project12/Project12/Form1.cs
@@ -72,10 +72,41 @@ namespace Project12
             else
             {
                 if (month > 1)
-                { 
-                    if(month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12)
+                {
+                    if (month == 2 || month == 4 || month == 6 || month == 8 || month == 9 || month == 11)
+                    {
+                        month = 4;
+                        countOfDaysPerMonth = 31;
+                        month--;
+                    }
+                    else if (month == 5 || month == 7 || month == 10 || month == 12)
+                    {
+                        month = 5;
+                        countOfDaysPerMonth = 30;
+                        month--;
+                    }
+                    else if (month == 3)
+                    {
+                        if (year % 4 == 0)
+                        {
+                            countOfDaysPerMonth = 29;
+                            month--;
+                        }
+                        else
+                        {
+                            countOfDaysPerMonth = 28;
+                            month--;
+                        }
+                    }
+                }
+                else
+                {
+                    day = 31;
+                    month = 12;
+                    year--;
                 }
             }
+            textBox5.Text = day + "." + month + "." + year;
         }
     }
 }
