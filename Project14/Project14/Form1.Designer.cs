bbe48d0d8628ea9d4ce0518473a8b687d5dd7b59
Avgustin16 <35662619+Avgustin16@users.noreply.github.com>
2018-11-13 15:43:53 +0200

    Add files via upload

---
+++ Project14/Project14/Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace Project14
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.checkBox6 = new System.Windows.Forms.CheckBox();
+            this.checkBox7 = new System.Windows.Forms.CheckBox();
+            this.checkBox8 = new System.Windows.Forms.CheckBox();
+            this.checkBox9 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(63, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(452, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Отбележете верните свойства на квадрата:";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(68, 93);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(183, 29);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "4 равни страни";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(68, 129);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(329, 29);
+            this.checkBox2.TabIndex = 2;
+            this.checkBox2.Text = "2 по 2 страните са успоредни";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(68, 165);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(283, 29);
+            this.checkBox3.TabIndex = 3;
+            this.checkBox3.Text = "диагоналите му са равни\r\n";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(68, 201);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(432, 29);
+            this.checkBox4.TabIndex = 4;
+            this.checkBox4.Text = "всичките му ъгли са прави(=90 градуса)";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Location = new System.Drawing.Point(68, 237);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(401, 29);
+            this.checkBox5.TabIndex = 5;
+            this.checkBox5.Text = "диагоналите му са перпендикулярни";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            // 
+            // checkBox6
+            // 
+            this.checkBox6.AutoSize = true;
+            this.checkBox6.Location = new System.Drawing.Point(68, 273);
+            this.checkBox6.Name = "checkBox6";
+            this.checkBox6.Size = new System.Drawing.Size(163, 29);
+            this.checkBox6.TabIndex = 6;
+            this.checkBox6.Text = "има 5 страни";
+            this.checkBox6.UseVisualStyleBackColor = true;
+            // 
+            // checkBox7
+            // 
+            this.checkBox7.AutoSize = true;
+            this.checkBox7.Location = new System.Drawing.Point(68, 309);
+            this.checkBox7.Name = "checkBox7";
+            this.checkBox7.Size = new System.Drawing.Size(313, 29);
+            this.checkBox7.TabIndex = 7;
+            this.checkBox7.Text = "диагоналите му не са равни";
+            this.checkBox7.UseVisualStyleBackColor = true;
+            // 
+            // checkBox8
+            // 
+            this.checkBox8.AutoSize = true;
+            this.checkBox8.Location = new System.Drawing.Point(68, 345);
+            this.checkBox8.Name = "checkBox8";
+            this.checkBox8.Size = new System.Drawing.Size(431, 29);
+            this.checkBox8.TabIndex = 8;
+            this.checkBox8.Text = "диагоналите му не са перпендикулярни";
+            this.checkBox8.UseVisualStyleBackColor = true;
+            // 
+            // checkBox9
+            // 
+            this.checkBox9.AutoSize = true;
+            this.checkBox9.Location = new System.Drawing.Point(68, 381);
+            this.checkBox9.Name = "checkBox9";
+            this.checkBox9.Size = new System.Drawing.Size(254, 29);
+            this.checkBox9.TabIndex = 9;
+            this.checkBox9.Text = "ъглите му не са прави";
+            this.checkBox9.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(587, 47);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 47);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Начало";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(587, 119);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 54);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Провери";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(587, 201);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(34, 31);
+            this.textBox1.TabIndex = 12;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(627, 205);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 25);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "точки";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(810, 566);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox9);
+            this.Controls.Add(this.checkBox8);
+            this.Controls.Add(this.checkBox7);
+            this.Controls.Add(this.checkBox6);
+            this.Controls.Add(this.checkBox5);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.CheckBox checkBox6;
+        private System.Windows.Forms.CheckBox checkBox7;
+        private System.Windows.Forms.CheckBox checkBox8;
+        private System.Windows.Forms.CheckBox checkBox9;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Project14/Project14/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Project14
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int points=0;
+            if (checkBox1.Checked == true)
+            {
+                points++;
+                checkBox1.ForeColor = Color.Blue;
+            }
+            else
+            {
+                checkBox1.ForeColor = Color.Red;
+            }
+            if (checkBox2.Checked == true)
+            {
+                points++;
+                checkBox2.ForeColor = Color.Blue;
+            }
+            else
+            {
+                checkBox2.ForeColor = Color.Red;
+            }
+            if (checkBox3.Checked == true)
+            {
+                points++;
+                checkBox3.ForeColor = Color.Blue;
+            }
+            else
+            {
+                checkBox3.ForeColor = Color.Red;
+            }
+            if (checkBox4.Checked == true)
+            {
+                points++;
+                checkBox4.ForeColor = Color.Blue;
+            }
+            else
+            {
+                checkBox4.ForeColor = Color.Red;
+            }
+            if (checkBox5.Checked == true)
+            {
+                points++;
+                checkBox5.ForeColor = Color.Blue;
+            }
+            else
+            {
+                checkBox5.ForeColor = Color.Red;
+            }
+            textBox1.Text = points.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            checkBox1.Checked = false;
+            checkBox1.ForeColor = Color.Black;
+            checkBox2.Checked = false;
+            checkBox2.ForeColor = Color.Black;
+            checkBox3.Checked = false;
+            checkBox2.ForeColor = Color.Black;
+            checkBox4.Checked = false;
+            checkBox4.ForeColor = Color.Black;
+            checkBox5.Checked = false;
+            checkBox5.ForeColor = Color.Black;
+            checkBox6.Checked = false;
+            checkBox6.ForeColor = Color.Black;
+            checkBox7.Checked = false;
+            checkBox7.ForeColor = Color.Black;
+            checkBox8.Checked = false;
+            checkBox8.ForeColor = Color.Black;
+            checkBox9.Checked = false;
+            checkBox9.ForeColor = Color.Black;
+        }
+    }
+}
