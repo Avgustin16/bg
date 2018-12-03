4c76dd0c18bf55415aadb0a77c033eb249b40664
Avgustin16 <35662619+Avgustin16@users.noreply.github.com>
2018-12-03 14:48:59 +0200

    Add files via upload

---
+++ Project21.1/Project21.1/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Project21._1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i <= 255; i++)
+            {
+                listBox1.Items.Add(i + " -> " + (char)i);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            for (char i = 'a'; i <= 'z'; i++)
+            {
+                listBox2.Items.Add(i + " -> " + (int)i);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string text = textBox1.Text;
+            for (int i = 0; i < text.Length; i++)
+            {
+                listBox3.Items.Add(text[i]);
+            }
+        }
+    }
+}
