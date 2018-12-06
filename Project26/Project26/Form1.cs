70712bc300da80ba53f556ccf3ff3838775271f8
Avgustin16 <35662619+Avgustin16@users.noreply.github.com>
2018-12-06 11:58:33 +0200

    Add files via upload

---
+++ Project26/Project26/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project26
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string text = textBox1.Text.ToLower();
+            textBox2.Text = text;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string text = textBox1.Text.ToUpper();
+            textBox3.Text = text;
+        }
+    }
+}
