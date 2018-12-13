b748e0d448b1018032313c052d7598935528d795
Avgustin16 <35662619+Avgustin16@users.noreply.github.com>
2018-12-13 15:41:41 +0200

    Add files via upload

---
+++ Project28/Project28/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Project28
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string text = textBox1.Text;
+            int i=0;
+            while (text[i]!=' ')
+            {
+                i++;
+            }
+            int k=i+1;
+            while(text[k]!=' ')
+            {
+                k++;
+            }
+            textBox2.Text = text.Remove(i, k - i);
+        }
+    }
+}
