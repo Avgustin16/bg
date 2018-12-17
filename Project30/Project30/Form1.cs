ece7f2b793eac6e022f952b50a7b5555aec79699
Avgustin16 <35662619+Avgustin16@users.noreply.github.com>
2018-12-17 14:50:01 +0200

    Add files via upload

---
+++ Project30/Project30/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Project30
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string text = textBox1.Text;
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] == '.')
+                {
+                    listBox1.Items.Add("съобщително");
+                }
+                else if (text[i] == '?')
+                {
+                    listBox1.Items.Add("въпросително");
+                }
+                else if (text[i] == '!')
+                {
+                    listBox1.Items.Add("възклицателно");
+                }
+                else if (text[i] == ',')
+                {
+
+                    textBox1.Text = text.Replace(',', ';');
+                }
+            }
+        }
+    }
+}
